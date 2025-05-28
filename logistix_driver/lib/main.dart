3526e91b1f85acedee484b166e583a03bcd7254e
Rohit Singh <rohit00710@outlook.com>
2025-05-28 22:54:41 +0530

    firebase service

--- logistix_driver/lib/main.dart
+++ logistix_driver/lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:logistix_driver/services/auth_service.dart';
 import 'package:logistix_driver/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
